9adbacdef1a31ee2b3688794930c3f6195ae1e7e
Lori <lori.tieshue@gmail.com>
2018-01-10 10:30:35 -0700

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner anything = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String inputFromUser = anything.nextLine();
+        System.out.println("Hello:" + inputFromUser);}
+
+
+//
+// if (inputFromUser.equals("password"))
+//        {
+//            System.out.println("Hello:" + inputFromUser);}
+//        else
+//            System.out.println("Try Again");}
+
+
+}
